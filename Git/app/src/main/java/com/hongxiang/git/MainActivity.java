c0606f2479b4ce416c0c1c4a98348902ee0762db
hongxiang <916634913@qq.com>
2016-05-09 09:54:23 +0800

    啊哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈

---
+++ Git/app/src/main/java/com/hongxiang/git/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hongxiang.git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     *
+     * @param savedInstanceState
+     * 哈哈哈哈哈哈哈
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
